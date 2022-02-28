bc25386ad374bf8a0dd5720030059200c6733d99
Vuong BUI <37221772+vuong-buihv@users.noreply.github.com>
2022-02-28 23:03:45 +0100

    JSON file parsed using serde_json (#18)

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::net::TcpStream;
 use std::str::from_utf8;
 use request_methods::RequestMethods;
 use args::Args;
+use serde_json::{Value};
 
 const DEFAULT_HOST: &str = "127.0.0.1";
 const DEFAULT_PORT: &str = "8080";
@@ -19,16 +20,18 @@ fn main() {
     let host = args.host.unwrap_or(DEFAULT_HOST.to_string());
     let port = args.port.unwrap_or(DEFAULT_PORT.to_string());
 
+    let json_content = fs::read_to_string(&args.json_filename).unwrap();
+    let json_value: Value = serde_json::from_str(&*json_content).unwrap();
+
     let listener = TcpListener::bind(format!("{}:{}", DEFAULT_HOST, port)).unwrap();
     println!("\tServer started at http://{}:{}", host, port);
 
-    let json_content = fs::read_to_string(args.json_filename).unwrap();
     for stream in listener.incoming() {
-        handle_connection(stream.unwrap(), &json_content);
+        handle_connection(stream.unwrap(), &json_value);
     }
 }
 
-fn handle_connection(mut stream: TcpStream, json_content: &String) {
+fn handle_connection(mut stream: TcpStream, json_value: &Value) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
@@ -37,7 +40,8 @@ fn handle_connection(mut stream: TcpStream, json_content: &String) {
 
     match request_type {
         RequestMethods::GET => {
-            let response = create_successful_response(json_content);
+            // TODO: very very naive implementation, it just sends the second post
+            let response = create_successful_response(&json_value["posts"][1].to_string());
 
             send_response(&stream, &response);
         },
@@ -60,11 +64,11 @@ fn handle_connection(mut stream: TcpStream, json_content: &String) {
 fn get_request_body(request: &str) -> String {
     let split_request: Vec<&str> = request.split("{").collect();
 
-    return "{".to_owned() + split_request[1];
+    "{".to_owned() + split_request[1]
 }
 
 fn create_successful_response(body: &str) -> String {
-    return format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", body.len(), body);
+    format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", body.len(), body)
 }
 
 fn send_response(mut stream: &TcpStream, response: &str){
